b4c942ee0f3ecc36e836027f675441d3c353f7a2
“robin <luohaozh@gmail.com>
2021-09-20 16:40:41 -0500

    update

--- fqf_iqn_qrdqn/agent/iqn_agent.py
+++ fqf_iqn_qrdqn/agent/iqn_agent.py
@@ -178,8 +178,8 @@ class IQNAgent(BaseAgent):
 
         target_sa_quantiles=target_sa_quantiles[:,torch.randperm(target_sa_quantiles.size(1))]
         current_sa_quantiles = current_sa_quantiles[:,torch.randperm(current_sa_quantiles.size(1))]
-        assert target_sa_quantiles.shape == (self.batch_size, self.N_dash, 1)
         assert current_sa_quantiles.shape == (self.batch_size, self.N, 1)
+        assert target_sa_quantiles.shape == (self.batch_size, self.N, 1)
 
         current_sa_quantiles_d = self.discriminator(current_sa_quantiles)
         target_sa_quantiles_d = self.discriminator(target_sa_quantiles)
