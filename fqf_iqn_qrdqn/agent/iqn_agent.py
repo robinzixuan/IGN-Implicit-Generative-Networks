db8c6dbe10271b7563c676d2b409b1ada7db67e1
“robin <luohaozh@gmail.com>
2021-09-22 11:07:51 -0500

    update

--- fqf_iqn_qrdqn/agent/iqn_agent.py
+++ fqf_iqn_qrdqn/agent/iqn_agent.py
@@ -189,11 +189,11 @@ class IQNAgent(BaseAgent):
         for p in self.discriminator.parameters():
             p.requires_grad = True
         
-        for i in range(self.n_critic):
-            self.discriminator.zero_grad()
-            GAN_loss = (current_sa_quantiles_d - target_sa_quantiles_d).mean().clone()
-            GAN_loss.backward(retain_graph=True)
-            self.discriminator_optim.step() 
+       
+        self.discriminator.zero_grad()
+        GAN_loss = (current_sa_quantiles_d - target_sa_quantiles_d).mean()
+        GAN_loss.backward(retain_graph=True)
+        self.discriminator_optim.step() 
         
         for p in self.discriminator.parameters():
             p.requires_grad = False  # to avoid computation
