b6f1f4d115ac6665406809093d9f13c602d7bb3a
“robin <luohaozh@gmail.com>
2021-09-28 19:38:24 -0500

    add

--- fqf_iqn_qrdqn/agent/iqn_agent.py
+++ fqf_iqn_qrdqn/agent/iqn_agent.py
@@ -39,9 +39,9 @@ class Discriminator(nn.Module):
         img_flat = img.view(img.shape[0], -1)
         batch_size = states.shape[0]
         action = torch.unsqueeze(action, dim=1).repeat(1, 64, 1)
-        #states = torch.unsqueeze(states, dim=1).repeat(1, 64, 1)
-        #states = states.repeat(1, 64, 1)
-        action = action.repeat(1,64,1)
+        action= action.reshape(batch_size * 64, *action.shape[2:])
+        states = torch.unsqueeze(states, dim=1).repeat(1, 64, 1)
+        states = states.reshape(batch_size * 64, *states.shape[2:])
         state_embeddings = self.dqn_net(states)
 
         print(img.shape)
