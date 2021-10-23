7eeea473289c65fed4547a6fbedfece5ec96e98a
“robin <luohaozh@gmail.com>
2021-10-23 14:37:54 -0500

    fix

--- train_iqn.py
+++ train_iqn.py
@@ -28,25 +28,15 @@ def run(args):
     agent_evaluation = IQNAgent(
         env=env, test_env=test_env, log_dir=log_dir, seed=args.seed,
         cuda=args.cuda, **config)
-<<<<<<< HEAD
-    model_dir = os.path.join(log_dir, 'model/saved')
-    filepath = ''
-    for root, directories, files in os.walk(model_dir):
-        for filename in files:
-            filepath = os.path.join(root, filename)
-    if os.path.exists(filepath):
-        agent_temp.load_models(filepath)
-=======
 
 
     # load model
-    agent_evaluation.load_models(os.path.join("result/PongNoFrameskip-v4/iqn-seed0-20211007-0313/model", "best"))
+    agent_evaluation.load_models(os.path.join(args.agent, "best"))
     print("Model Load done.", args.env_id)
 
     # 
     print("Start policy evaluation...")
 
->>>>>>> 89948973a1ee80d1ab842ed694d5e81391f590a7
     agent = IQNAgent(
         env=env, test_env=test_env, log_dir=log_dir, seed=args.seed,
         cuda=args.cuda, agent=agent_evaluation, env_online=env_online, **config)
@@ -62,6 +52,7 @@ if __name__ == '__main__':
     parser.add_argument('--cuda', type=bool, default=True)
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--agent', type=str, default="result/PongNoFrameskip-v4/iqn-seed0-20211007-0313/model")
     args = parser.parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
     run(args)
